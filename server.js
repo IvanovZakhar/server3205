76d547244ae6707624644bbff85f932649e86bd2
Zakhar <uvanov977777@gmail.com>
2023-07-13 11:12:53 +0300

    update cors

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  
+    next();
+  });
 let currentSearchRequest = null;
 
 // Обработка POST-запроса на поиск
